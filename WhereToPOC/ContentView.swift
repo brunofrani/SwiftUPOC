5eebd62fc2523bcc3409ed178067be135e902960
Bruno Frani <bruno.frani@mentormate.com>
2024-01-19 11:43:41 +0100

    Initial Commit

---
+++ WhereToPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhereToPOC
+//
+//  Created by Bruno Frani on 19.1.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
